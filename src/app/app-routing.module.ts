963b4b1eb05ad92121b155938fa09c47b326bb41
Toan Nguyen <toan.1aus@gmail.com>
2022-01-03 08:11:33 +1000

    Show child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryCountComponent } from './core/categoryCount.component';
 import { NewProductComponent } from "./core/new-product.component";
 import { NotFoundComponent } from "./core/notFound.component";
+import { ProductCountComponent } from './core/productCount.component';
 import { TableComponent } from "./core/table.component";
 
 const routes: Routes = [
   { path: "product/:mode/:id", component: NewProductComponent },
   { path: "product/:mode", component: NewProductComponent },
   { path: "table/:category", component: TableComponent },
-  { path: "table", component: TableComponent },
+  {
+    path: "table", component: TableComponent,
+    children: [
+      { path: "products", component: ProductCountComponent },
+      { path: "categories", component: CategoryCountComponent }
+    ]
+  },
   { path: "", redirectTo: "/table", pathMatch: "full" },
   { path: "**", component: NotFoundComponent }];
 
