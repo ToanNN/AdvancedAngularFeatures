b41d429e5e54e9cfd03c6c20edbb747d4a092de0
Toan Nguyen <toan.1aus@gmail.com>
2022-01-02 14:41:18 +1000

    Use reactive to handle events

--- src/app/core/new-product.component.ts
+++ src/app/core/new-product.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { filter, Observable } from "rxjs";
 import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
-import { MODES, SharedState } from "./sharedstate.model";
+import { MODES, SharedState, SHARED_STATE } from "./sharedstate.model";
 
 @Component({
   selector: "new-product",
@@ -10,14 +11,19 @@ import { MODES, SharedState } from "./sharedstate.model";
   styleUrls: ["new-product.component.css"]
 })
 export class NewProductComponent {
-  product: Product;
-  constructor(private model: Model, private state: SharedState) {
-    this.product = new Product();
+  product: Product = new Product();
+  constructor(private model: Model, @Inject(SHARED_STATE) public stateEvents: Observable<SharedState>) {
+    stateEvents.pipe(filter(state => state.id != 3)).
+      subscribe(update => {
+        this.product = new Product();
+        if (update.id != undefined) {
+          Object.assign(this.product, this.model.getProduct(update.id));
+        }
+        this.editing = update.mode == MODES.EDIT;
+      })
   }
 
-  get editing(): boolean {
-    return this.state.mode === MODES.EDIT;
-  }
+  editing: boolean = false;
 
   submitForm(form: NgForm) {
     if (form.valid) {
@@ -28,6 +34,6 @@ export class NewProductComponent {
   }
   resetForm() {
     this.product = new Product();
-    this.state.mode = MODES.CREATE;
+
   }
 }
